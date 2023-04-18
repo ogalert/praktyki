7777201f89eb6a645f7d5bcbc16420980b722643
Oliwier <130741989+ogalert@users.noreply.github.com>
2023-04-18 15:14:10 +0200

    Update users.sql

--- login/users.sql
+++ login/users.sql
@@ -1,2 +1,2 @@
-CREATE TABLE Users (ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE (username));
-INSERT INTO `Users` (`ID`, `username`, `password`) VALUES (NULL, 'admin', 'test')
+CREATE TABLE users (ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE (username));
+INSERT INTO `users` (`ID`, `username`, `password`) VALUES (NULL, 'admin', 'test')
