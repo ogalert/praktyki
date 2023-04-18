cb41a0e33aecbdd86a1ba3237e710d8b6d2f4311
Oliwier <130741989+ogalert@users.noreply.github.com>
2023-04-18 13:03:46 +0200

    zadanie 3
    i też zmiana formularza z get na post

--- login/index.php
+++ login/index.php
@@ -1,19 +1,26 @@
 <?php
-//ekran logowania w PHP
-if (isset($_GET['submit'])) {
-    //sprawdzanie, czy nazwa użytkownika/login to admin
-    if($_GET['login'] === "admin") {
-        //sprawdzanie, czy hasło użytkownika to "test"
-        if($_GET['haslo'] === "test") {
-            echo "Witamy, ".$_GET['login'];
-        }
-        else {
-            echo "<h1> Błędne hasło! </h1>";
-        }
-    } else {
-        echo"<h1> Błędny login! </h1>";
+$connect = new mysqli("localhost", "root", "", "users");
+
+if ($connect->connect_error) {
+    echo "Błąd połączenia z bazą danych: " . $connect->connect_error;
+    exit();
+}
+
+if (isset($_POST['submit'])) {
+    $login = $_POST['login'];
+    $pass = $_POST['haslo'];
+
+    $sql = "SELECT * FROM users WHERE username = '$login' and password = '$pass'";
+    $result = mysqli_query($connect, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "Witamy, ".$_POST['login'];
+    }
+    else {
+        echo "<h3>Niepoprawne hasło lub login.</h3>";
     }
 }
+mysqli_close($connect);
 ?>
 
 <!DOCTYPE html>
@@ -22,10 +29,10 @@ if (isset($_GET['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadanie 2 - PHP Login</title>
+    <title>Zadanie 2 i 3 - PHP Login</title>
 </head>
 <body>
-    <form method="get" action="">
+    <form method="post" action="">
       <p> Login: </p> <input type="text" value="admin" name="login" id=""> <br>
        <p> Hasło </p> <input type="password" value="test" name="haslo" id=""> <br>
         <input type="submit" name="submit" value="Zaloguj się">
